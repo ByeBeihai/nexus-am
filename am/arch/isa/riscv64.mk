d710fee3b1e209f185adefc53c0ca7ca3ef32d12
ByeBeihai <2637039533@qq.com>
2022-11-12 14:38:51 +0000

    use gcc at begining

--- am/arch/isa/riscv64.mk
+++ am/arch/isa/riscv64.mk
@@ -1,5 +1,5 @@
 CROSS_COMPILE := riscv64-linux-gnu-
-COMMON_FLAGS  := -fno-pic -march=rv64gcp0p96_zpsfoperand0p96_zpn0p96_zbp0p93_zbt0p93_zbb  -mcmodel=medany -menable-experimental-extensions -mno-relax
+COMMON_FLAGS  := -fno-pic -march=rv64g -mcmodel=medany -mno-relax
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
 LDFLAGS       += -melf64lriscv 
--- apps/pext/src/random.c
+++ apps/pext/src/random.c
@@ -1,9 +1,12 @@
 #include <klib.h>
 int main() {
-for (int i = 0; i < 1; ++i) {
-int a = 6;
-int b = 5;
-uint64_t c =__rv_add8(a,b);
-printf("c = %d add16 test pass!\n",c,i+1);
-}
+    for (int i = 0; i < 1; ++i) {
+        int a = 6;
+        int b = 5;
+        int c = __rv_add16(a,b); //CALL Add16 instr
+        if(c == 11)
+            printf("c = %d add16 test pass!\n",c);
+        else
+            printf("c = %d error！\n",c);
+    }
 }
